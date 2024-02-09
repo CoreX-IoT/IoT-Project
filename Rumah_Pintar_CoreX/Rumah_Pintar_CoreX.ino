5f7023314a9241009f4353be511e21504b88d6b3
DUWIPANGGA <129023021+DUWIPANGGA@users.noreply.github.com>
2024-02-09 10:07:32 +0700

    Rumah_Pintar_CoreX.ino

--- Rumah_Pintar_CoreX/Rumah_Pintar_CoreX.ino
+++ Rumah_Pintar_CoreX/Rumah_Pintar_CoreX.ino
@@ -1,6 +1,6 @@
 /*
 URL Project: https://github.com/CoreX-IoT/IoT-Project/tree/production/Rumah_Pintar_CoreX
-Autor: Duwipangga
+Author: Duwipangga
 Page: https://corex.id/project/Rumah_pintar_coreX
 Tutorial: https://blog.corex.id/membuat-smart-home-menggunakan-corex
 */
